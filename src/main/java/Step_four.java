0203d45ab03d0e8f141391b88afca66a549aa8a0
Kseniia Martin <62717016+Xeniia-m@users.noreply.github.com>
2021-09-02 13:33:20 +0300

    Update Step_four.java

--- src/main/java/Step_four.java
+++ src/main/java/Step_four.java
@@ -20,7 +20,6 @@ public class Step_four {
     public static class MapperClass extends Mapper<LongWritable, Text,  Text, Text> {
 
         private double get_pmi(double N, double bigram_num, double first_word_num, double second_word_num) {
-            System.out.println("Log(80)= "+Math.log10(80.0));
             return (Math.log10(bigram_num) + Math.log10(N) - Math.log10(first_word_num) - Math.log10(second_word_num));
         }
 
@@ -37,6 +36,7 @@ public class Step_four {
 
         }
 /*
+    map gets: 
     decade  word1 word2     N   c(word1,word2)     c(word1 *)   c(* word2)
  */
         @Override
@@ -47,16 +47,14 @@ public class Step_four {
             double first_word_num = Double.parseDouble(vals[5]);
             double second_word_num = Double.parseDouble(vals[6]);
             double N = Double.parseDouble(vals[3]);
-            System.out.println("bigram_num: "+bigram_num+" first: "+first_word_num+ " second:"+second_word_num+" N:"+N);
             double pmi = get_pmi(N, bigram_num, first_word_num, second_word_num);
             double npmi = get_npmi(pmi, N, bigram_num);
-            System.out.println("map npmi "+npmi);
-            context.write(new Text(0+" "+decade),new Text(Double.toString(npmi)));
+           
+            context.write(new Text(0+" "+decade),new Text(Double.toString(npmi))); //for second condition check
             double minPmi = Double.parseDouble(context.getConfiguration().get("minPmi", "1"));
-            System.out.println("decade: "+decade+ " bigram: "+vals[1]+" "+vals[2]+"\n   pmi: "+pmi+" nmpi: "+ npmi);
-            System.out.println("min_pmi: "+minPmi);
+            
+            //first condition:
             if (npmi >= minPmi) {
-                System.out.println("first condition");
                 context.write(new Text(1+" "+decade+" "+(1-npmi)+" "+npmi),new Text(vals[1]+" "+vals[2]+" 1"));
             }
             else {
@@ -79,25 +77,21 @@ public class Step_four {
            String[] key_ = key.toString().split(" ");
             if(key_[0].equals("0")){
                 for(Text value: values){
-                   // String[] vals = value.toString().split(" ");
                     double num = Double.parseDouble(value.toString());
-
                     count+=num;
-                    //System.out.println("sum_npmi "+Double.toString(count));
-
+                  
                 }
-               // context.write(new Text(key_[1]),new Text(Double.toString(count)));
+               
             }
             else {
-                //context.write(new Text(key_[1]),new Text(":"));
-                // System.out.println("sum_npmi "+Double.toString(count));
                 for (Text value : values) {
-
                     String[] vals = value.toString().split(" ");
+                    
+                    //check if the first condition is passed
                     if (vals[2].equals("1")) {
                      context.write(new Text("decade:" + key_[1] + " " + vals[0]), new Text(vals[1] + "\t" + key_[3]));
-
                     }
+                    //if not we need to check the second condition
                     else {
                         double npmi = Double.parseDouble(key_[3]);
                         double rel_minPmi = Double.parseDouble(context.getConfiguration().get("rel_minPmi", "1"));
@@ -120,7 +114,7 @@ public class Step_four {
         public void cleanup(Context context) throws IOException, InterruptedException {
         }
     }
-
+    //evaluates the npmi value in the same decade
     public static class CombinerClass extends Reducer<Text,Text, Text, Text> {
 
         @Override
@@ -129,11 +123,11 @@ public class Step_four {
             String[] key_ = key.toString().split(" ");
             if (key_[0].equals("0")) {
                 for (Text value : values) {
-                    // String[] vals = value.toString().split(" ");
+                    
                     double num = Double.parseDouble(value.toString());
 
                     count += num;
-                    //System.out.println("sum_npmi "+Double.toString(count));
+                   
 
                 }
                 context.write(key, new Text(Double.toString(count)));
@@ -178,19 +172,7 @@ public class Step_four {
         if (state4 != 0) {
             System.exit(1);
         }
-      /*  aws aws_ = new aws();
-        aws_.create_s3();
-        aws_.create_sqs();
-        System.out.println("    Good collocations:");
-        aws_.print_messages("goodCollocations");
-        System.out.println("    Bad collocations:");
-        aws_.print_messages("badCollocations");
-*/
-        // aws_.delete_folder("dsass2bgu","output1");
-       // aws_.delete_folder("dsass2bgu","output2");
-       // aws_.delete_folder("dsass2bgu","output3");
-
-
+     
     }
 
 }
